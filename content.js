0316b02b5ed373de1c54a06b92a0002f0331e3c7
melissajwen <wenm@usc.edu>
2017-04-01 14:50:49 -0700

    reworked sentiment analysis to run in background based on google search queries
    discard testform.html

--- content.js
+++ content.js
@@ -1,28 +1,30 @@
-function sentimentAnalysis() { 
-    var _data = {
-        "documents": [
-            {
-                "language": "en",
-                "id": "1",
-                "text": $("#s_text").val(),
-            }
-        ]
+var temp = document.title;
+var query = temp.substring(0, temp.length-16);
+
+var json = {
+  "documents": [
+    {
+      "language": "en",
+      "id": "string",
+      "text": query
     }
+  ]
+};
 
-    $.ajax({
-        type: "POST",
-        url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
-        headers: {
-            "Ocp-Apim-Subscription-Key": "fa306418c4994f8eaddf7396d40e8ec3 ", 
-            "Content-Type": "application/json", 
-            "Accept": "application/json"
-        },
-        data: JSON.stringify(_data),
-        success: function(data) {
-            score = data["documents"][0]["score"];
-            console.log(score);
-        },
-    });
-
-    return false;
-}
+$.ajax({
+    type: "POST",
+    url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
+    headers: {
+        "Ocp-Apim-Subscription-Key": "fa306418c4994f8eaddf7396d40e8ec3 ", 
+        "Content-Type": "application/json", 
+        "Accept": "application/json"
+    },
+    data: JSON.stringify(json),
+    success: function(data) {
+        score = data["documents"][0]["score"];
+        console.log(score);
+    },
+    fail: function() {
+        console.log("error");
+    }
+});
