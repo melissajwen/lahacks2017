736aad209c19872bce74993f84877b3b665041fa
New User <newuser@News-MacBook-Pro.local>
2017-04-02 06:32:05 -0700

    pull and pushed

--- content.js
+++ content.js
@@ -1,12 +1,6 @@
-// var query;
-// setInterval(function() {
-//     var temp = document.title;
-//     query = temp.substring(0, temp.length-16);
-//     console.log(query);
-// }, 500);
-
 var temp = document.title;
 var query = temp.substring(0, temp.length-16);
+var score = 0;
 console.log(query);
 
 // Call the Microsoft Sentiment Analysis API
@@ -33,46 +27,57 @@ $.ajax({
     success: function(data) {
         score = data["documents"][0]["score"];
         console.log(score);
+        if (score < 0.6) {
+            sendMessage(query);
+        }
     },
 });
 
-// var twilio = require('twilio');
+function ml()
 
-// // Find your account sid and auth token in your Twilio account Console.
-// var TWILIO_ACCOUNT_SID = "AC9445ee82c7dc710745168742c8d3aa78";
-// var TWILIO_AUTH_TOKEN = "42b6698f202fe43f70b29e8bd5e901c6";
-// var YOUR_NUMBER = "+15039262778";
-// var YOUR_TWILIO_NUMBER = "+19803524225";
-// var client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-
-// // Send the text message.
-// client.sendMessage({
-//   to: YOUR_NUMBER,
-//   from: YOUR_TWILIO_NUMBER,
-//   body: 'Hello from Twilio!'
-// });
+function sendMessage(query) {
+    var script = $.ajax({
+        type: "POST",
+        url: "http://127.0.0.1:5000/send_sms",
+        dataType: "text",
+        data: { param: query },
+        success: console.log("sent a text to the number because sentiment score is too low")
+    });
+}
 
 
-// Link Twilio API in Python
+// function runPython(query){
+    
 
-// $.ajax({
-//     type: "POST",
-//     url: "~/send_sms.py",
-//     data: { 
-//         param: query
-//     },
-//     success: function(data){
-//         alert(data);
-//     }
-// });
+//     return 0;
+// }
 
-// If some condition is fulfilled, send to ML
-// ML condition is fulfilled, send to Twilio
+// Following function performs machine learning algorithm on queries with scores of < 0.6
+
+//if (score < 0.6) {
+    // var twilio = 'twilio';
+    // var client;
+    // require([twilio], function(twilio) {
+    //     client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+    // });
+    // //var twilio = require('twilio');
+
+    // console.log(client);
+    // // Find your account sid and auth token in your Twilio account Console.
+    // var TWILIO_ACCOUNT_SID = "AC9445ee82c7dc710745168742c8d3aa78";
+    // var TWILIO_AUTH_TOKEN = "42b6698f202fe43f70b29e8bd5e901c6";
+    // var YOUR_NUMBER = "+15039262778";
+    // var YOUR_TWILIO_NUMBER = "+19803524225";
+
+    // // Send the text message.
+    // client.messages.create({
+    //   to: YOUR_NUMBER,
+    //   from: YOUR_TWILIO_NUMBER,
+    //   body: 'Hello from Twilio!'
+    // });
 
 
-
-
-
+//}
 
 
 
---
+++ mltest.py
@@ -0,0 +1,167 @@
+
+# coding: utf-8
+
+# In[216]:
+
+get_ipython().magic('matplotlib inline')
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
+
+from datetime import datetime
+import itertools
+
+
+# In[217]:
+
+data = pd.read_csv('test.csv')
+data.head()
+
+
+# In[218]:
+
+data[data['text'] == 'I want to kill myself']['bool'] = 0
+data
+
+
+# In[219]:
+
+data.info()
+data.describe()
+
+
+# In[220]:
+
+negative_words = np.array(['hate', 'annoy', 'anxious', 'bad', 
+                           'corrupt', 'cut', 'crazy', 'dead', 'die', 'depressed',
+                          'dreadful', 'evil', 'fear', 'foul', 'frighten', 'gross',
+                          'angry', 'ill', 'insane', 'injure', 'jealous', 'malicious',
+                          'mean', 'monstrous', 'nobody', 'nonsense', 'pain', 'poor',
+                          'quit', 'rage', 'rude', 'rotten', 'reject', 'repulsive', 'savage',
+                          'scary', 'severe', 'sucks', 'ugly', 'terrible', 'upset', 'vile',
+                          'wicked', 'freak', 'curse', 'not', 'cannot', 'no', 'monster',
+                          'trapped', 'burden', 'hopeless', 'gun', 'suicide', 'pills', 'overdose', 'never',
+                          'imperfect', 'disapprove', 'hard', 'challenging', 'demanding', 'different'
+                          'blame', 'painless', 'kill'])
+
+
+# In[221]:
+
+data
+
+
+# ## random branch of other stuff
+
+# In[222]:
+
+from sklearn import feature_extraction
+
+vectorizer = feature_extraction.text.CountVectorizer(vocabulary=negative_words)
+X = vectorizer.fit_transform(data['text'])
+
+
+
+# In[223]:
+
+X = X.todense()
+X
+
+
+# In[224]:
+
+y = data['bool']
+
+
+# In[225]:
+
+# equivalent:
+# data['bool']
+# data.bool
+
+
+# In[226]:
+
+X.shape
+
+
+# In[227]:
+
+np.hstack((X,data['score'].reshape((27,1)).shape))
+
+
+# In[228]:
+
+X_train, X_test, y_train, y_test  = train_test_split(X, y,
+    train_size=0.50, 
+    random_state=420)
+
+
+# In[229]:
+
+model = LogisticRegression()
+
+
+# In[230]:
+
+model.fit(X_train,y_train)
+
+
+# In[231]:
+
+model.predict_proba(X_test)
+
+
+# In[232]:
+
+model.coef_
+
+
+# In[178]:
+
+X_test[0]
+
+
+# ## end random branch
+
+# In[108]:
+
+def create_vector(string, counter):
+    temp = []
+    splitString = string.split(" ")
+    negCount = 0
+
+    for nword in negative_words:
+        temp = np.append(temp, np.array([splitString.count(nword)]))
+        if splitString.count(nword) > 0:
+            negCount = 1
+    
+    temp = np.append(temp, np.array([data.score[counter]]))
+    
+    return np.array(temp,negCount) 
+
+        
+
+
+# In[104]:
+
+#handling the parent interaction (input as the text string)
+
+def unflag(string,X):
+    vec = create_vector(string)
+    for v,i in enumerate(X):
+        if np.array_equal(v, vec[0]):
+            y[i] = 0
+    
+
+
+# In[105]:
+
+#testing flagging
+# unflag('I hate you and you a monstrous nobody')
+
